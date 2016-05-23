a284bc798ad79200be70e8e0a72b45d18efff51c
roryphillipsjcu <rory.phillips@my.jcu.edu.au>
2016-05-23 11:32:52 +1000

    Re added footer

--- footer.php
+++ footer.php
@@ -15,9 +15,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bro' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bro' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'https://google.com/', 'bro' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bro' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php // printf( esc_html__( 'Theme: %1$s by %2$s.', 'bro' ), 'bro', '<a href="http://underscores.me/" rel="designer">FlyingMongoose</a>' ); ?>
+			<?php  printf( esc_html__( 'Theme: %1$s by %2$s.', 'bro' ), 'bro', '<a href="http://underscores.me/" rel="designer">FlyingMongoose</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
